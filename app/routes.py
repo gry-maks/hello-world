53bf6a91a186bd5d9c0bc4d499f2b8e4d82cf98d
websolut <serggry@yahoo.com>
2019-09-07 13:10:19 +0100

    first flask application

---
+++ app/routes.py
@@ -0,0 +1,25 @@
+from app import app
+
+@app.route('/')
+@app.route('/index')
+def index():
+    user = {'username': 'Miguel'}
+    return '''
+<html>
+    <head>
+        <title>Home Page - Microblog</title>
+    </head>
+    <body>
+        <h1>Hello, ''' + user['username'] + '''!</h1>
+    </body>
+</html>'''
+
+@app.route("/login")
+def login():
+    return "login page"
+
+
+
+@app.route('/users')
+def users():
+    return 'User page'
